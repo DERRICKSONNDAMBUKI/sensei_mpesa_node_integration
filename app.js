89fd6db7284bc423351dfe9dafb10ff88ec58021
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-26 12:15:25 +0300

    authorization commit

--- app.js
+++ app.js
@@ -8,47 +8,37 @@ app.get("/", (req, res) => {
   res.send("Hello sensei");
 });
 
-app.get("/access_token", (req, res) => {
-  // access token
-
-  //   let unirest = require("unirest");
-  //   let req = unirest(
-  //     "GET",
-  //     "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
-  //   )
-  //     .headers({
-  //       Authorization:
-  //         "Bearer cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ==",
-  //     })
-  //     .send()
-  //     .end((res) => {
-  //       if (res.error) throw new Error(res.error);
-  //       console.log(res.raw_body);
-  //     });
-  let url =
-    "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
-  let auth = new Buffer.from(
-    "CH7CjKtxwa62bqk4AMmAFPMW17rsPT9e:zgzZMnK1KD3VtuPB"
-  ).toString("base64");
-
-  request(
-    {
-      url: url,
-      headers: {
-        'Authorization' : 'Basic '+auth
-      //   Authorization:
-      //     `Bearer cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ== CH7CjKtxwa62bqk4AMmAFPMW17rsPT9e:zgzZMnK1KD3VtuPB`,
-      },
-    },
-    (error, response, body) => {
-      if (error) {
-        console.error(error);
-      } else {
-        res.status(200).json(body);
-      }
-    }
-  );
+app.get("/access_token",access, (req, res) => {
+ 
+  res.status(200).json({access_token})
 });
+
+const access =(req,res,next)=>{
+  let url =
+  "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+let auth = new Buffer.from(
+  "CH7CjKtxwa62bqk4AMmAFPMW17rsPT9e:zgzZMnK1KD3VtuPB"
+).toString("base64");
+
+request(
+  {
+    url: url,
+    headers: {
+      'Authorization' : 'Basic '+auth
+    //   Authorization:
+    //     `Bearer cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ== CH7CjKtxwa62bqk4AMmAFPMW17rsPT9e:zgzZMnK1KD3VtuPB`,
+    },
+  },
+  (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      res.status(200).json(body);
+    }
+  }
+);
+
+}
 
 // listen
 app.listen(8000, (err, live) => {
