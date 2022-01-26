6f9c7714cc123fdf9825d6c3be4c864e1efae363
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-01-26 13:50:27 +0300

    registering routes commit

--- app.js
+++ app.js
@@ -8,37 +8,63 @@ app.get("/", (req, res) => {
   res.send("Hello sensei");
 });
 
-app.get("/access_token",access, (req, res) => {
- 
-  res.status(200).json({access_token})
-});
 
-const access =(req,res,next)=>{
+const access = (req, res, next) => {
+  // access token
   let url =
-  "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
-let auth = new Buffer.from(
-  "CH7CjKtxwa62bqk4AMmAFPMW17rsPT9e:zgzZMnK1KD3VtuPB"
-).toString("base64");
+    "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+  let auth = new Buffer.from(
+    "CH7CjKtxwa62bqk4AMmAFPMW17rsPT9e:zgzZMnK1KD3VtuPB"
+  ).toString("base64");
 
-request(
-  {
-    url: url,
-    headers: {
-      'Authorization' : 'Basic '+auth
-    //   Authorization:
-    //     `Bearer cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ== CH7CjKtxwa62bqk4AMmAFPMW17rsPT9e:zgzZMnK1KD3VtuPB`,
+  request(
+    {
+      url: url,
+      headers: {
+        Authorization: "Basic " + auth,
+      },
     },
-  },
-  (error, response, body) => {
-    if (error) {
-      console.error(error);
-    } else {
+    (error, response, body) => {
+      if (error) {
+        console.error(error);
+      } else {
+        req.access_token = JSON.parse(body).access_token;
+        next();
+      }
+    }
+  );
+};
+
+app.get("/register", access, (req, res) => {
+  let url = "";
+  let auth = "Bearer " + req.access_token;
+  request(
+    {
+      url: "",
+      method: "POST",
+      headers: {
+        'Authorization':auth
+      },
+      json:{
+        'ShortCode':'174379',
+        'ResponseType':'Complete',
+        'ConfirmationURL':'http://192.168.22.47:801/confirmation',
+        'ValidationURL':'http://192.168.22.47:801/validation_url'
+      }
+    },
+    (error, response, body) => {
+      if (error) {
+        console.error(error);
+      }
       res.status(200).json(body);
     }
-  }
-);
+  );
+});
 
-}
+
+app.get("/access_token", access, (req, res) => {
+  res.status(200).json({ access_token: req.access_token });
+});
 
 // listen
 app.listen(8000, (err, live) => {
